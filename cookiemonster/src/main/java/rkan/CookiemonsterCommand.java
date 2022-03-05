c9125856659c90ce7599d214e8b0043d3d00264c
Rahul <rahukand@gmail.com>
2022-03-04 16:21:51 -0800

    CLI program works!!

--- cookiemonster/src/main/java/rkan/CookiemonsterCommand.java
+++ cookiemonster/src/main/java/rkan/CookiemonsterCommand.java
@@ -3,26 +3,40 @@ package rkan;
 import io.micronaut.configuration.picocli.PicocliRunner;
 import io.micronaut.context.ApplicationContext;
 
+import io.micronaut.context.annotation.Parameter;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
 @Command(name = "cookiemonster", description = "...",
         mixinStandardHelpOptions = true)
 public class CookiemonsterCommand implements Runnable {
 
-    @Option(names = {"-v", "--verbose"}, description = "...")
-    boolean verbose;
+    @Parameters(paramLabel = "FILE", description = "The file containing logs to read")
+    File logFile;
+
+    @Option(names = {"-d", "--date"}, description = "The date for the most active log")
+    LocalDate date;
+
 
     public static void main(String[] args) throws Exception {
         PicocliRunner.run(CookiemonsterCommand.class, args);
     }
 
     public void run() {
-        // business logic here
-        if (verbose) {
-            System.out.println("Hi!");
+        try {
+            ArrayList<Cookie> cookies = CookieMonster.mostActiveCookie(logFile, date);
+            String cookieString = cookies.stream().map(c -> c.getCOOKIE()+'\n').sorted().collect(Collectors.joining());
+            System.out.println(cookieString);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
     }
 }
